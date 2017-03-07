f0305beb01e7d0b56b22f2343df5bee9bf893cff
Istvan Marki <istv0050@edu.easj.dk>
2017-03-07 19:23:08 +0100

    uploading rest of the files

---
+++ ComicBooksChallengeASP/Models/ComicBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBooksChallengeASP.Models
+{
+    public class ComicBook
+    {
+        public int ComicBookId { get; set; }
+        public string ComicTitle { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string EpisodeTitle { get; set; }
+
+
+    }
+
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+    }
+}
