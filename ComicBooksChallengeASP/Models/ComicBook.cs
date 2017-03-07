011ee4fb1abd2fb6e1fac7dd8a2f8d0ea9459459
Istvan Marki <istv0050@edu.easj.dk>
2017-03-07 19:35:07 +0100

    finishing up model

--- ComicBooksChallengeASP/Models/ComicBook.cs
+++ ComicBooksChallengeASP/Models/ComicBook.cs
@@ -11,6 +11,7 @@ namespace ComicBooksChallengeASP.Models
         public string ComicTitle { get; set; }
         public int EpisodeNumber { get; set; }
         public string EpisodeTitle { get; set; }
+        public List<Character> Characters { get; set; }
 
 
     }
@@ -19,5 +20,18 @@ namespace ComicBooksChallengeASP.Models
     {
         public int CharacterId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class ComicBookManager
+    {
+        public static List<ComicBook> GetComicBooks()
+        {
+            return new List<ComicBook>
+            {
+                new ComicBook{ComicBookId = 1, ComicTitle = "Andreas And", EpisodeNumber = 56, EpisodeTitle = "Ugly ducklings", Characters = new List<Character>{ new Character{CharacterId = 1, Name = "Andreas the first"}}},
+                new ComicBook{ComicBookId = 2, ComicTitle = "Supper Dinner Lunch", EpisodeNumber = 1, EpisodeTitle = "Fox beats duck", Characters = new List<Character>{ new Character{CharacterId = 1, Name = "Andreas the first"}, new Character{CharacterId = 2, Name = "FoxyMaxi"}}},
+                new ComicBook{ComicBookId = 3, ComicTitle = "Badoo", EpisodeNumber = 42, EpisodeTitle = "Hungry Again", Characters = new List<Character>{ new Character{CharacterId = 2, Name = "FoxyMaxi"}}}
+            };
+        }
     }
 }
